32658522ae3d9bcf29f7893f9a2d04baa2d0b381
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-06-27 19:37:12 +0530

    Modify webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,30 +3,47 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: {
-    index: './src/index.js',
-
+  entry: './src/index.js',
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
-  devtool: 'inline-source-map',
+  optimization: {
+    runtimeChunk: 'single',
+  },
   devServer: {
     static: './dist',
+    watchFiles: ['./src/*'],
+    open: true,
+    hot: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
-
     }),
   ],
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
   module: {
     rules: [
       {
-        test: /\.css$/i,
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader'],
+        include: /fontawesome-free/,
+      },
+      {
+        test: /\.css$/,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
       },
     ],
   },
