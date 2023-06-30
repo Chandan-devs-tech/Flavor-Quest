7a342a0fcd760c041841176452d8145a2b3c6e87
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-06-30 22:44:06 +0600

    Add functionality to fetch comments from API and display

--- src/modules/commentPopUp.js
+++ src/modules/commentPopUp.js
@@ -1,14 +1,15 @@
 /* eslint-disable no-console */
-import setComment from './setComment.js';
+import { setComment, getComments, displayComment } from './setComment.js';
 
 const popupWindow = async (id) => {
   const mainContainer = document.querySelector('.main-container');
   const result = await fetch(
     `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`,
   );
+  
   const data = await result.json();
   const mealDetails = data.meals[0];
-  console.log(mealDetails);
+  // console.log(mealDetails);
   const overlay = document.createElement('div');
   overlay.classList.add('overlay');
 
@@ -35,44 +36,58 @@ const popupWindow = async (id) => {
   const popupFoodName = document.createElement('h3');
   popupFoodName.textContent = `${mealDetails.strMeal}`;
 
-  const commentHeader = document.createElement('h4');
-  commentHeader.className = 'comment-header';
-  commentHeader.innerHTML = 'Comments (Counter coming soon)';
-
-  const userCommentsDiv = document.createElement('div');
-  userCommentsDiv.classList.add('comments-container');
-  userCommentsDiv.textContent = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.';
-
+  
+  
   const formTitle = document.createElement('div');
   formTitle.className = 'comment-input-sec';
   formTitle.textContent = 'Add a comment';
-
+  
   const form = document.createElement('form');
   form.className = 'form';
-
+  
   const nameInput = document.createElement('input');
   nameInput.className = 'name-input';
   nameInput.type = 'text';
   nameInput.placeholder = 'Your Name';
-
+  
   const yourInsightInput = document.createElement('textarea');
   yourInsightInput.className = 'your-insight';
   yourInsightInput.cols = 40;
   yourInsightInput.rows = 6;
   yourInsightInput.placeholder = 'Your Insights';
-
+  
   const submit = document.createElement('button');
   submit.className = 'submit';
   submit.innerText = 'Submit';
+  
+  const commentHeader = document.createElement('h4');
+  commentHeader.className = 'comment-header';
+  // commentHeader.innerHTML = '(Counter coming soon) Comments';
+  const userCommentsDiv = document.createElement('div');
+  userCommentsDiv.classList.add('comments-container');
+  // userCommentsDiv.textContent = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.';
+  
+  // // set comments to API starts
+  // submit.addEventListener('click', async (event) => {
+  //   event.preventDefault();
+  //   const nameVal = nameInput.value;
+  //   const insightVal = yourInsightInput.value;
+  //   setComment(id, nameVal, insightVal);
+  //   const comments12 = await getComments(id);
+  //   console.log('This is comments12', comments12);
+  //   displayComment(comments12, userCommentsDiv);
+  // });
+  
+  const comments12 = await getComments(id);
+  displayComment(comments12, userCommentsDiv, commentHeader);
 
-  // set comments to API starts
-  submit.addEventListener('click', (event) => {
-    event.preventDefault();
-    const nameVal = nameInput.value;
-    const insightVal = yourInsightInput.value;
-    setComment(id, nameVal, insightVal);
-  });
   // set comments to API done
+
+  // get comments from API starts
+
+  // getComments(`${involvementCommentUrl}?item_id=${id}`);  
+  
+  // get comments from API done
 
   form.appendChild(nameInput);
   form.appendChild(yourInsightInput);
@@ -81,18 +96,29 @@ const popupWindow = async (id) => {
   popupContainer.appendChild(closeButton);
   popupContainer.appendChild(popupImage);
   popupContainer.appendChild(imgDescipDiv);
-  popupContainer.appendChild(commentHeader);
-  popupContainer.appendChild(userCommentsDiv);
   popupContainer.appendChild(formTitle);
   popupContainer.appendChild(form);
+  popupContainer.appendChild(commentHeader);
   overlay.appendChild(popupContainer);
   mainContainer.appendChild(overlay);
+  popupContainer.appendChild(userCommentsDiv);
 
   const closePopup = () => {
     mainContainer.removeChild(overlay);
   };
 
   closeButton.addEventListener('click', closePopup);
+
+  // set comments to API starts
+  submit.addEventListener('click', async (event) => {
+    event.preventDefault();
+    const nameVal = nameInput.value;
+    const insightVal = yourInsightInput.value;
+    setComment(id, nameVal, insightVal);
+    const comments12 = await getComments(id);
+    console.log('This is comments12', comments12);
+    displayComment(comments12, userCommentsDiv, commentHeader);
+  });
 };
 
 export default popupWindow;
--- src/modules/setComment.js
+++ src/modules/setComment.js
@@ -19,20 +19,55 @@ const setComment = async (param1, param2, param3) => {
         },
       });
 
-      if (response.ok) {
-        console.log('Comment submitted successfully');
-        console.log(commentData);
-        alert('Your comment is received.');
-      } else {
-        alert('Please fill all the fields.');
-      }
+      // if (response.ok) {
+      //   console.log('Comment submitted successfully');
+      //   console.log(commentData);
+      //   alert('Your comment is received.');
+      // } else {
+      //   alert('Please fill all the fields.');
+      // }
     } catch (error) {
       console.error('Error submitting comment', error);
     }
   } else if (!param2 || !param3) {
     alert('Please fill all the fields.');
-    console.error('Error submitting comment');
   }
 };
 
-export default setComment;
+const getComments = async (id) => {
+  // try {
+    const options = {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    };
+    const response = await fetch(`${involvementCommentUrl}?item_id=${id}`, options);
+
+    const comments = response.json();
+    console.log('This is the response', comments);
+    return comments;
+  // } catch (error) {
+  //   console.error('Error fetching comments', error);
+  // }
+};
+
+// const getComments = async (id) => {
+//   const result = await fetch(`${involvementCommentUrl}?item_id=${id}`);
+//   const { comment } = await result.json();
+//   return comment;
+// };
+
+const displayComment = (commentsArray, param, param1) => {
+  param.innerHTML = '';
+  commentsArray.forEach(element => {
+    param.innerHTML += `<div>${element.creation_date} ${element.username} 'Says,' ${element.comment}</div>`;
+    param1.innerHTML = `Total comment (${commentsArray.length})`;
+    // param1.textContent = element.comment;
+  });
+  // const [username, comment] = commentsArray;
+  // param.textContent = comment;
+  // param1.textContent = username;
+}
+
+export { setComment, getComments, displayComment };
