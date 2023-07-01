985433d8fda3be335c1999b56c1c06c2470d7e53
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-07-02 03:02:21 +0530

    Add test for Items counter

---
+++ dist/modules/apiData.js
@@ -0,0 +1,12 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.involvementLikeUrl = exports.involvementCommentUrl = exports.baseUrl = void 0;
+var baseUrl = 'https://www.themealdb.com/api/json/v1/1/filter.php?a=American';
+exports.baseUrl = baseUrl;
+var involvementLikeUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/nAowKIQaetz8oNeFtqjs/likes/';
+exports.involvementLikeUrl = involvementLikeUrl;
+var involvementCommentUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/nAowKIQaetz8oNeFtqjs/comments/';
+exports.involvementCommentUrl = involvementCommentUrl;
